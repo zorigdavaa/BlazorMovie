85d63754d944a8c0ea058d52ebb2dd74e3bcb30c
zorigdavaa <54620719+zorigdavaa@users.noreply.github.com>
2020-05-03 16:25:25 +0800

    End of Routing

--- BlazorMovie/Models/Movie.cs
+++ BlazorMovie/Models/Movie.cs
@@ -7,6 +7,7 @@ namespace BlazorMovie.Models
 {
     public class Movie
     {
+        public int Id { get; set; } = 1;
         public string Title { get; set; }
         public DateTime ReleaseDate { get; set; }
         public string Poster { get; set; }
